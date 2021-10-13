5ebf1fb03f883727951a85f1beb8db2a11438163
NoNotCar <13haslat@gmail.com>
2021-10-13 16:55:11 +0100

    Sympy for objective/jacobian

--- scratch.py
+++ scratch.py
@@ -1,10 +1,14 @@
+import sympy
+
 import dynamics
 import numpy as np
 import scipy.optimize as optimise
+import sympy as sym
 
 def optimise_trajectory(n=20,T=2.0,max_force=20.0,max_disp=2.0,silent=False):
     dt = T/n
-    # decision variables x = [s_0[0]...s_0[3],f_0...]
+    decision_variables = sympy.symbols(" ".join(f"x_{t} theta_{t} v_{t} omega_{t} force_{t}" for t in range(n)))
+    decision_array = sympy.Array(decision_variables)
     smxf = np.square(max_force)
     smxd = np.square(max_disp)
 
@@ -12,8 +16,7 @@ def optimise_trajectory(n=20,T=2.0,max_force=20.0,max_disp=2.0,silent=False):
         return x[t*5:t*5+4]
     def f(x,t):
         return dynamics.x_dot(s(x,t), x[t * 5 + 4])
-    def objective(x):
-        return 0.5*(np.square(x[4])+np.square(x[-1]))+sum(np.square(f) for f in x[9:-1:5])
+    objective = 0.5*(decision_variables[4]**2+decision_variables[-1]**2)+sum(f**2 for f in decision_variables[9:-1:5])
     def colloc_constraint(x,t):
         return 0.5*dt*(f(x,t)+f(x,t+1))-s(x,t+1)+s(x,t)
     def force_constraint(x,t):
@@ -27,6 +30,7 @@ def optimise_trajectory(n=20,T=2.0,max_force=20.0,max_disp=2.0,silent=False):
     def final_angvel_constraint(x):
         return x[-2]
 
+    jacobian = sym.derive_by_array(objective,decision_array)
     x0 = np.linspace(np.array([0,0,0,0,0]),np.array([max_disp,np.pi,0,0,0]),n)
     x0 = x0.flatten()
     collocs = [{"type":"eq","fun":colloc_constraint,"args":[t]} for t in range(n-1)]
@@ -38,7 +42,11 @@ def optimise_trajectory(n=20,T=2.0,max_force=20.0,max_disp=2.0,silent=False):
         {"type": "eq", "fun": final_angvel_constraint},
         {"type": "eq", "fun": lambda x: x[0:4]}
     ]
-    result=optimise.minimize(objective,x0,constraints=collocs+forces+finals)
+    result=optimise.minimize(
+        sym.lambdify([decision_variables],objective,"numpy"),
+        x0,
+        jac=sym.lambdify([decision_variables],jacobian,"numpy"),
+        constraints=collocs+forces+finals)
     if not silent:
         print(result)
     return result["x"]
